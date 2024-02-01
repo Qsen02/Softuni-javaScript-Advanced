28503f3a3fd90eb65339bc26687b43ca44d2320b
Qsen02 <diyangenchev73@gmail.com>
2024-02-01 21:03:04 +0200

    Update unitTestCheckForSymmetry.js

--- Unit Testing and Error Handling-lab/Check for Symmetry/unitTestCheckForSymmetry.js
+++ Unit Testing and Error Handling-lab/Check for Symmetry/unitTestCheckForSymmetry.js
@@ -25,4 +25,16 @@ describe("Suit", function() {
     it("works", function() {
         expect(isSymmetric(["1", "2", "3", "3", "2", "1"])).to.equal(true);
     });
+    it("works", function() {
+        expect(isSymmetric([1, "2", 3, "3", 2, "1"])).to.equal(false);
+    });
+    it("works", function() {
+        expect(isSymmetric(["-1", "2", "-3", "3", "-2", "1"])).to.equal(false);
+    });
+    it("works", function() {
+        expect(isSymmetric(["1.1", "2.2", "3.3", "3.3", "2.2", "1.1"])).to.equal(true);
+    });
+    it("works", function() {
+        expect(isSymmetric(["-1", "2", "-3", "-3", "2", "-1"])).to.equal(true);
+    });
 })
